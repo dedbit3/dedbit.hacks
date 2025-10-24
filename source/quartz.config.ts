57942880263b48935a55b47c417d324425ead156
Your Name <you@example.com>
2025-10-23 23:15:42 -0400

    fix title

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "👾 Dedbit Hacks Writeups",
+    pageTitle: "👾 Dedbit Writeups",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
